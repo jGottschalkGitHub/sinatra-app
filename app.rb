233f8f456227cfa484b3e1347f480dfd60416a2b
Julia Gottschalk <yizaigottschalk@gmail.com>
2018-04-24 15:25:31 +0200

    added session secret in app.rb

--- app.rb
+++ app.rb
@@ -6,17 +6,21 @@ require 'pry'
 require 'digest'
 require 'better_errors'
 require_relative 'lib/routes'
+require 'sinatra/reloader'
 
 class Todo < Sinatra::Application
   set :environment, :development
   register Sinatra::Flash
   enable :sessions
+  set :session_secret, 'ASLKJ124LSDFK30912'
 
   configure do
+    register Sinatra::Reloader
+    also_reload 'models/*'
     # DB = Sequel.connect("mysql2://root:pass@mysql.getapp.docker/todo")
   end
   env = ENV['RACK_ENV'] || 'development'
-  DB = Sequel.connect(YAML.load(File.open('database.yml'))[env])
+  DB = Sequel.connect(YAML.safe_load(File.open('database.yml'))[env])
 
   Sequel::Model.raise_on_save_failure = false
   # Sequel plugins loaded by ALL models.
--- backup_006_alter_items_table.rb
+++
@@ -1,8 +0,0 @@
-Sequel.migration do 
-    change do 
-        alter_table :items do
-            add_column(:due_date, DateTime)
-            add_column(:starred, TrueClass)   
-        end 
-    end 
-end 
