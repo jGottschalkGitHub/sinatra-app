2ac7a6979c1d25648075560fd8c35cc15c50000a
Julia Gottschalk <yizaigottschalk@gmail.com>
2018-04-24 11:35:14 +0200

    migrations rubocop compliant2

--- db/migrations/006_create_comments_table.rb
+++ db/migrations/006_create_comments_table.rb
@@ -2,9 +2,9 @@ Sequel.migration do
   change do
     create_table :comments do
       primary_key :id
-      foreign_key :user_id 
-      foreign_key :list_id 
-      String :text, length: 256 
+      foreign_key :user_id
+      foreign_key :list_id
+      String :text, length: 256
       DateTime :creation_date
     end
   end
